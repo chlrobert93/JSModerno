2d2497689193c0dcde3e1ea687a9f144c1a984f8
Eder <chlrobert93@gmail.com>
2021-05-16 20:27:02 -0500

    eliminar console.log

--- js/app.js
+++ js/app.js
@@ -1,10 +1,1 @@
-console.time('Hola');
 
-console.log('Eso no esta permitido');
-console.log('Eso no esta permitido');
-console.log('Eso no esta permitido');
-console.log('Eso no esta permitido');
-console.log('Eso no esta permitido');
-console.log('Eso no esta permitido');
-
-console.timeEnd('Hola');
