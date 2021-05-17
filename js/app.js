4d7b1625c90821c298fe676400a2aa5c22a0d353
Eder <chlrobert93@gmail.com>
2021-05-16 21:12:13 -0500

    Crear string js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
+const producto = "Monitor 20\"";
+const producto2 =String('Monitor 24" ');
 
+//Se le concoe como crear onjecto de tipo string
+const producto3 = new String('Monitor 27 Pulgadas');
+
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
