74e426064769d07a03c30f14e54d8996869cdbf4
Eder <chlrobert93@gmail.com>
2021-05-16 21:33:04 -0500

    Metodos include-legth

---
+++ js/metodosDeCadena.js
@@ -0,0 +1,10 @@
+const producto = "Monitor 20 Pulgadas";
+console.log(producto);
+
+//Conocer la cantidad de letras de la cadena de texto
+console.log(producto.length);
+
+
+console.log(producto.indexOf('Pulgadas'));
+
+console.log(producto.includes('Tablet'));
