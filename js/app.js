f20be5645992574d9e69a51e05c8eda968516994
Eder <chlrobert93@gmail.com>
2021-05-16 20:25:47 -0500

    Variables var

--- js/app.js
+++ js/app.js
@@ -1,10 +1,42 @@
-console.time('Hola');
+//Inicializar una variable con un valor
+var producto = "Monitor de 24 pulgadas";
+console.log(producto);
 
-console.log('Eso no esta permitido');
-console.log('Eso no esta permitido');
-console.log('Eso no esta permitido');
-console.log('Eso no esta permitido');
-console.log('Eso no esta permitido');
-console.log('Eso no esta permitido');
 
-console.timeEnd('Hola');
+// Las variables se puedenreasignar
+producto = "Monitor de 19 pulgadas";
+console.log(producto);
+
+//JavaScript es un lenguaje de tipo dinamico,no se especifica tipo de dato
+producto= 20;
+console.log(producto);
+
+//Se puede inicializar sin valor y asegurarlo después
+var disponibilidad;
+disponibilidad = true;
+
+diponibilidad = false;
+
+
+//Inicializar múltiples variables
+var categoria = 'Computadoras',
+    marca = "Marca famosa";
+    calificación = 5;
+
+
+//Las variables no pueden inicializar con números
+var dias99;
+var __usuario;
+var _01;
+
+//Kamel Keys 
+var nombreProducto
+
+//Wonder
+var nombre_producto;
+
+//Serpiente
+var nombre_producto_categoria_precio;
+
+//Pascal cais para clasess
+var NombreProducto
