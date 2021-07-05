429bd1dbe79279b86c0866e97448495cd4684075
Eder <chlrobert93@gmail.com>
2021-07-05 11:16:48 -0500

    operaciones JS

---
+++ js/operacionesJS.js
@@ -0,0 +1,24 @@
+
+//Operaciones JS
+
+const numero1 = 30;
+const   numero2 = 20;
+
+let resultado;
+
+//Suma
+resultado = numero1 + numero2;
+
+//Multiplicación
+resultado = numero1 + numero2;
+
+//Divición
+resultado = numero1 + numero2;
+
+//Modulo
+resultado = numero1 % numero2;
+
+console.log(resultado);
+
+
+//Math
