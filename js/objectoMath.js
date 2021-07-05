d9d42d5678d955519b72f51770f614512920d0f1
Eder <chlrobert93@gmail.com>
2021-07-05 11:33:33 -0500

    objectoMath

---
+++ js/objectoMath.js
@@ -0,0 +1,48 @@
+let resultado;
+
+
+//PI
+resultado = Math.PI;
+
+
+//Redondear 
+
+resultado = Math.round(2.8);
+resultado = Math.round(2.2);
+resultado = Math.round(2.6);
+resultado = Math.round(2.5);
+resultado = Math.round(2.4);
+
+
+//Redondear hacia arriba
+resultado = Math.ceil(2.1);
+
+//Redondear hacia abajo
+resultado = Math.floor(2.9);
+
+//Raiz cuadrada
+resultado = Math.sqrt(144);
+
+//Absoluto
+resultado = Math.abs(-500);
+
+//Potencia
+resultado = Math.pow(2,4);
+
+//Minimo
+resultado = Math.min(34,5,35,66,);
+
+//Maximo
+resultado = Math.max(33,44,55,3);
+
+
+//Aleatorio
+resultado = Math.random() * 20;
+
+
+//Aleatorio dentro de un rango..
+resultado = Math.floor(Math.random() * 30);
+
+resultado = Math.round(Math.random() * 30);
+
+console.log(resultado);
