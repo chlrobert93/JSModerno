35bb005fb689e03449f2127796f3efc932d8b121
Eder <chlrobert93@gmail.com>
2021-08-25 00:15:50 -0500

    Como acceder a los valores de un objecto

---
+++ js/accederaunObjecto.js
@@ -0,0 +1,16 @@
+const producto = {
+ 
+    nombre: "Monitor 20",
+    precio: 300,
+    disponible: true,
+}
+
+
+
+console.log(producto);
+
+console.log(producto.nombre);
+console.log(producto.precio);
+console.log(producto.disponible);
+
+console.log(producto['nombre']);
