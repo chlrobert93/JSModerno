6088d88a296d563a54de41396cce7a7ed0346316
Eder <chlrobert93@gmail.com>
2021-08-24 22:13:26 -0500

    Crear objecto en JS

---
+++ js/crearObjecto.js
@@ -0,0 +1,17 @@
+
+const nombre = "Monitor";
+const precio = 300;
+const disponible = true;
+
+
+//Un objecto agrupa todo en una sola variable...
+
+
+//Objecto literal
+const producto = {
+    nombre: "Monitor 20 pulgadas",
+    precio: 300,
+    disponibilidad: true,
+}
+
+console.log(producto);
