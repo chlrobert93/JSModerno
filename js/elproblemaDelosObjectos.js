452fea7b07fb1b8b1cffdd484556c0b8f14f1bc9
Eder <chlrobert93@gmail.com>
2021-08-25 01:49:53 -0500

    El problema de los objectos

---
+++ js/elproblemaDelosObjectos.js
@@ -0,0 +1,13 @@
+//Se puede reasignar un objecto
+
+
+const producto = {
+  nombre: "Monitor 20 pulgadas",
+  precio: 300,
+  disponible: true,
+}
+
+producto.disponible = false;
+delete producto.precio;
+
+console.log(producto);
