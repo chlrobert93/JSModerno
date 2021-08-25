6f8d5f5db8ffb881fbc59ed1a98761be11b5b14d
Eder <chlrobert93@gmail.com>
2021-08-25 00:38:10 -0500

    Agregar o eliminar propiedades de un objecto

---
+++ js/agregarEliminarPropObjecto.js
@@ -0,0 +1,15 @@
+const producto = {
+ 
+    nombre: "Monitor 20",
+    precio: 300,
+    disponible: true,
+}
+
+
+//Agregar nuevas propiedades al objecto
+producto.imagen = "imagen.jpg";
+
+//Eliminar propiedades del objecto
+delete producto.disponible;
+
+console.log(producto);
