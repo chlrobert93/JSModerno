8ef9e8bfcd4f5b042557440cf8679aa90a7f628f
Eder <chlrobert93@gmail.com>
2021-08-31 23:19:35 -0500

    Congelar un Objecto para no poderlo modificar

---
+++ js/congelarUnObjectoParaNoModificar.js
@@ -0,0 +1,19 @@
+//No permitir malas practicas en este archivo
+"use stric"
+
+const producto = {
+     nombre: "Monitor 20Pulgadas",
+     preacio: 3000,
+     disponible: true,
+}
+
+//Toma el objecto para que no se modificado
+Object.freeze(producto);
+
+//producto.disponible = false;
+//producto.image = "image.jpg";
+delete producto.precio;
+console.log(producto);
+
+//Conocer si esta congelado
+console.log(Object.isFrozen(producto));
