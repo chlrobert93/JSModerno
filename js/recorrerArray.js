4b1683e0ec687df05a8ea28066c43983a8d1b13c
Eder <chlrobert93@gmail.com>
2021-09-02 22:55:06 -0500

    Recorrer un Array

---
+++ js/recorrerArray.js
@@ -0,0 +1,15 @@
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+
+
+console.log(meses);
+console.log(meses[0]);
+console.log(meses[1]);
+console.log(meses[2]);
+
+
+//Cunanto mideel arreglo
+console.log(meses.length);
+
+for(let i = 0; i < meses.length; i++){
+    console.log(meses[i]);
+}
