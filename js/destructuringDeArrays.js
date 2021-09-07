d23ebd05baa881e57697def9d0c7f623ee09c35c
Eder <chlrobert93@gmail.com>
2021-09-06 21:49:03 -0500

    Destructuring de Arrays

---
+++ js/destructuringDeArrays.js
@@ -0,0 +1,22 @@
+const producto = {
+  nombre: "Monitor 20 Pulgadas",
+  precio: 300,
+  disponible: true,
+};
+
+//const nombre = producto.nombre;
+//console.log(nombre);
+
+//Destructuring
+const { disponible } = producto;
+
+console.log(disponible);
+
+//Destructuring con Arreglos
+const numeros = [10, 20, 30, 40, 50];
+
+//const [,,,, quinto] = numeros;
+const [primero, ...tercero] = numeros;
+
+//console.log(quinto);
+console.log(tercero);
