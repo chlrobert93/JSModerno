5f6f15d55a63121bd163ba190b1adb77a2446671
Eder <chlrobert93@gmail.com>
2021-09-09 09:54:52 -0500

    Parametros y argumentos en funciones

---
+++ js/parametrosArgumentosEnFunciones.js
@@ -0,0 +1,13 @@
+function sumar(a, b) {
+  console.log(a + b);
+}
+
+sumar(2, 3);
+sumar(200, 189);
+
+function saludar(nombre, apellido) {
+  console.log(`Hola ${nombre} ${apellido}`);
+}
+
+saludar("Juan", "De la torre");
+saludar();
