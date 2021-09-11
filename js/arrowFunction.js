dc1266d6936942efa732dc03064e8dbdfcd43413
Eder <chlrobert93@gmail.com>
2021-09-10 21:53:21 -0500

    Arrow Function

---
+++ js/arrowFunction.js
@@ -0,0 +1,10 @@
+const aprendiendo = function () {
+  console.log("Aprendiendo Js");
+};
+
+//const aprendiendo2 = () => console.log("Aprendiendo JS");
+
+const aprendiendo2 = () => 'Aprendieno JS'
+
+console.log(aprendiendo2());
+//aprendiendo2();
