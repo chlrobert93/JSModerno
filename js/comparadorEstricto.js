f19eae5faf7552df81c7596d6794d929fe32352b
Eder <chlrobert93@gmail.com>
2021-09-11 00:15:31 -0500

    Comparador Estricto

---
+++ js/comparadorEstricto.js
@@ -0,0 +1,23 @@
+const puntaje = 1000;
+const puntaje2 = "1000";
+
+console.log(typeof puntaje);
+console.log(typeof puntaje2);
+
+//if(puntaje != 1000) { // != diferente == igual a
+  //console.log("si! es diferente");
+//}
+
+
+//if(!puntaje){
+  //console.log('gg');
+//}
+
+if(puntaje === "1000"){ //!= diferente == igual a
+  console.log('si es igual');
+}else{
+  console.log('No es igual');
+}
+
+
+//== operador  que no es estricto
