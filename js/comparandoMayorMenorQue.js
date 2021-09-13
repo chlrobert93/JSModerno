25b40e42db2913cbeb3db11fe061c1497ae07570
Eder <chlrobert93@gmail.com>
2021-09-12 22:26:48 -0500

    Comparando Mayor o Menor que

---
+++ js/comparandoMayorMenorQue.js
@@ -0,0 +1,10 @@
+//Operador Mayor que y menor que
+
+const dinero = 500;
+const totalApagar = 300;
+
+if (dinero >= totalApagar) {
+  console.log("Si podemospagar");
+} else {
+  console.log("Fondos Insuficientes");
+}
