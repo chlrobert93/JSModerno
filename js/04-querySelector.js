0cd111504f13583364e91316aa2d21fe9cdca2c4
Roberto Eder Chavarria <chlrobert93@gmail.com>
2021-11-23 00:53:59 -0600

    querySelector

---
+++ js/04-querySelector.js
@@ -0,0 +1,22 @@
+
+//Retornar máximo 1 elemento
+//querySelector seleciona classe, id 
+const card = document.querySelector('.card');
+console.log(card);
+
+
+//Se puede tener selectores especificos como css
+const info = document.querySelector('.premium .info');
+console.log(info);
+
+//Seleccionar el segundo card de hospedaje
+const segundoCard = document.querySelector('section.hospedaje .card:nth-child(2 )');
+console.log(segundoCard);
+
+//Seleccionar el formulario
+const formulario = document.querySelector('#formulario');
+console.log(formulario);
+
+//Seleccionar elemento HTML
+const navegacion = document.querySelector('nav');
+console.log(navegacion);
