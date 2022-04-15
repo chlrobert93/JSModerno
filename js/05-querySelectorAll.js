c315dd82dbe2d9c7839a0f547fb6d197f0d99a3a
Roberto Eder Chavarria <chlrobert93@gmail.com>
2022-04-15 11:40:07 -0500

    querySelectorAll

---
+++ js/05-querySelectorAll.js
@@ -0,0 +1,14 @@
+//Retorna todos los elementos que cumplan la condición
+
+const card = document.querySelectorAll('.card');
+console.log(card);
+
+const formulario = document.querySelectorAll('#formulario');
+console.log(formulario);
+
+//Si un elemento no existe
+const noExiste = document.querySelectorAll('no-existe');
+console.log(noExiste);
+
+const img = document.querySelectorAll('img');
+console.log(img);
